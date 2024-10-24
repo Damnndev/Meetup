6bc3f7a99c749834b40c89c5d8be79bd1b620d0b
Demian <92870546+Damnndev@users.noreply.github.com>
2024-10-24 11:17:14 +0200

    cleanup dummy data

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,12 +1,13 @@
 import { Stack } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
+import React from 'react';
 
 import { ScreenContent } from '~/components/ScreenContent';
 
 export default function Home() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Tab Two' }} />
+      <Stack.Screen options={{ title: 'Tab To' }} />
       <View style={styles.container}>
         <ScreenContent path="app/(tabs)/two.tsx" title="Tab Two" />
       </View>
